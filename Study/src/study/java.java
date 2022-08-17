89da53da367136eeb499fd9a84d7be6cf1c5bade
jeongjiwon <jeongjiwon@localhost>
2022-08-17 22:16:56 +0900

    java study

---
+++ Study/src/study/java.java
@@ -0,0 +1,19 @@
+package study;
+
+import java.util.Scanner;
+
+public class java {
+	
+	public static void study(String[] args) {
+		
+				
+				Scanner in = new Scanner(System.in);
+				int A = in.nextInt();
+				int B = in.nextInt();
+				
+				System.out.println(A+B);
+				
+				in.close();
+	}
+	}
+
