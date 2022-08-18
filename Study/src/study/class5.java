3948a3d9765105f82f04c2341105887a16e2d191
jeongjiwon <cmils@naver.com>
2022-08-18 22:36:39 +0900

    두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.

---
+++ Study/src/study/class5.java
@@ -0,0 +1,21 @@
+package study;
+
+import java.util.Scanner;
+
+public class class5 {
+
+	public static void Main(String[] args) {
+		
+	Scanner in = new Scanner(System.in);
+	
+	double a = in.nextDouble();
+	double b = in.nextDouble();
+	
+	in.close();
+	System.out.print(a/b);
+	
+
+
+	}
+
+}
