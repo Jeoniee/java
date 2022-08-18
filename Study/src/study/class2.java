d35514a6928a9c0e022e6bcceb8ff3dd845fc56d
jeongjiwon <jeongjiwon@localhost>
2022-08-18 21:37:44 +0900

    java0818

--- Study/src/study/class2.java
+++ Study/src/study/class2.java
@@ -1,10 +1,17 @@
 package study;
 
+import java.util.Scanner;
+
 public class class2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A+B);
+		
+		in.close();
 	}
 
 }
