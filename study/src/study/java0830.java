c080414230400e7ae3b59db5069707be3cb29269
ITWILL <cmils@naver.com>
2022-08-30 14:18:03 +0900

    첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.472 * 385

---
+++ study/src/study/java0830.java
@@ -0,0 +1,23 @@
+package study;
+
+import java.util.Scanner;
+
+public class java0830 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+			int a = in.nextInt();
+			int b = in.nextInt();
+			
+			in.close();
+			
+			
+				
+					
+	
+	
+	
+	}
+
+}
