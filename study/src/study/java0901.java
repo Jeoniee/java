e0751b151e31ecdc1356a90ef62861dcbb297943
ITWILL <cmils@naver.com>
2022-09-01 11:01:04 +0900

    강아지 출력 |"^"`    | ||_/=\\__|

---
+++ study/src/study/java0901.java
@@ -0,0 +1,20 @@
+package study;
+
+import java.util.Scanner;
+
+public class java0901 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.print(	"|\\_/|\n" +
+							"|q p|   /}\n" +
+							"( 0 )\"\"\"\\\n" +
+							"|\"^\"`    |\n" +
+							"||_/=\\\\__|\n");
+	}
+
+}
