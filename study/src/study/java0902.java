78f2d7d5cd2ddb1d398d985fc81bbe69d31d25b1
ITWILL <cmils@naver.com>
2022-09-02 14:52:28 +0900

    새싹 출력하기

---
+++ study/src/study/java0902.java
@@ -0,0 +1,22 @@
+package study;
+
+import java.util.Scanner;
+
+public class java0902 {
+
+	public static void main(String[] args) {
+
+			
+
+			Scanner in = new Scanner(System.in);
+			
+			
+			System.out.print("         ,r\'\"7\n" +
+					"r`-_   ,\'  ,/\n" +
+					 " \\. \". L_r'\n" +
+					   "   `~\\/\n" +
+					      "      |\n" +
+					      "      |");
+		}
+
+	}
