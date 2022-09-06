504f74702023c30ae847293328a9a932953d9327
jeongjiwon <cmils@naver.com>
2022-09-06 20:52:11 +0900

    msg

--- Study/src/study/java0822.java
+++ Study/src/study/java0822.java
@@ -1,10 +1,17 @@
 package study;
 
+import java.util.Scanner;
+
 public class java0822 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner(System.in);
+		String a = "??!";
+		String s = in.next();
+		
+		System.out.println(s + a);
+				
 	}
 
 }
