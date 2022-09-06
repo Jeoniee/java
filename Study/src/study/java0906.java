b53a88d14df151e0eefa33507afe1bfeb5d44309
jeongjiwon <jeongjiwon@localhost>
2022-09-06 20:46:48 +0900

    윤년 구하기.

---
+++ Study/src/study/java0906.java
@@ -0,0 +1,20 @@
+package study;
+
+import java.util.Scanner;
+
+public class java0906 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int Y = in.nextInt();
+		in.close();
+		
+		if(Y%4==0) {
+			if(Y%400==0) System.out.println("1");
+			else if(Y%100==0) System.out.println("0");
+			else System.out.println("1");
+		}else System.out.println("0");
+		}
+	}
+
