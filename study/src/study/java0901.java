a2686f03886cae907546eaa321805c7f715aebc8
ITWILL <cmils@naver.com>
2022-09-07 14:18:31 +0900

    v

--- study/src/study/java0901.java
+++ study/src/study/java0901.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class java0901 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		Scanner in = new Scanner(System.in);
 		
