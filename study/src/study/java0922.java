f1ab61f51459735a526d8ba7a61096a38f77e154
ITWILL <cmils@naver.com>
2022-09-22 17:08:49 +0900

    a+b-7

---
+++ study/src/study/java0922.java
@@ -0,0 +1,14 @@
+package study;
+
+import java.util.Scanner;
+
+public class java0922 {
+
+public static void main(String[] args) {
+ 
+
+	Scanner in = new Scanner(System.in);
+	int a = in.nextInt();
+	}
+}
+
